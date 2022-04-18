4cdc46ea6d4333f26bf63fb8f04d9d21377b9234
Alexandre Picinato Scherer <83470126+ElMaxh0@users.noreply.github.com>
2022-04-18 14:59:09 -0300

    Update statusLogin.js

--- src/global/hooks/Autenticator/statusLogin.js
+++ src/global/hooks/Autenticator/statusLogin.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState } from "react";
 export const LoginStatunsContext = createContext();
 export default function LoginStatunsProvider({children}){
-    const[LoginStatuns, setLoginStatuns]= useState(1);
+    const[LoginStatuns, setLoginStatuns]= useState(0);
     return <LoginStatunsContext.Provider value={{LoginStatuns,setLoginStatuns}}>{children}</LoginStatunsContext.Provider>
 }
 export function useLoginStatuns(){
